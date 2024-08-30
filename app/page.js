0c171e09a6b459092ca9cd48b5d936075276838b
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2024-08-30 14:43:56 -0400

    Back and forth messages

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 'use client';
-import {Box,Stack} from '@mui/material';
+import {Box, Button, Stack, TextField} from '@mui/material';
 import {useState} from 'react';
 
 export default function Home() {
@@ -7,7 +7,44 @@ export default function Home() {
     role: 'assistant',
     content: `Hi, I'm the HeadStarter Support Agent. How can I help you today?`,
   }]);
+
   const [message,setmessage]=useState('');
+
+  const sendmessage=async ()=>{
+    setmessage('');
+    setmessages((messages)=>[
+        ...messages,
+        {role:'user',content:message},
+        {role:'assistant',content:''},
+    ]);
+    const response=fetch('/api',{
+      method:'POST',
+      headers: {
+        'Content-Type':'application/json',
+      },
+      body: JSON.stringify([...messages,{role:'user',content:message}]),
+    }).then(async (res)=>{
+      const reader=res.body.getReader();
+      const decoder=new TextDecoder();
+      let result='';
+      return reader.read().then(function processText({done,value}){
+        if(done){return result;}
+        const text=decoder.decode(value||new Int8Array(),{stream:true});
+        setmessages((messages)=>{
+          let lastmsg=messages[messages.length-1];
+          let othermsgs=messages.slice(0,messages.length-1);
+          return[
+            ...othermsgs,
+            {
+              ...lastmsg,
+              content: lastmsg.content+text,
+            },
+          ]
+        });
+        return reader.read().then(processText);
+      });
+    });
+  }
 
   return <Box width='100vw' height='100bh' display='flex' flexDirection='column' justifyContent='center' alignItems='center'>
     <Stack direction='column' width='600px' height='700px' border='1px solid black' p={2} spacing={2}>
@@ -19,6 +56,10 @@ export default function Home() {
               </Box>
             </Box>
         ))}
+      </Stack>
+      <Stack direction='row' spacing={2}>
+        <TextField label='message' fullWidth value={message} onChange={(e)=>setmessage(e.target.value)}></TextField>
+        <Button variant='contained' onClick={sendmessage}>Send</Button>
       </Stack>
     </Stack>
   </Box>
